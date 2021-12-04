c099151c7b5b7f80886da50dfa527676ac604192
Szymon Hudziak <szymonh.dg@gmail.com>
2021-12-04 17:45:39 +0100

    Add displaying data from keypad

--- src/main.cpp
+++ src/main.cpp
@@ -2,35 +2,92 @@
 #include <LiquidCrystal.h>
 #include <Keypad.h>
 
+const byte ROWS = 3; //number of rows
+const byte COLS = 3; //number of columns
 
-const byte ROWS = 3; // ile wierszy
-const byte COLS = 3; //ile kolumn
- 
-byte rowPins[ROWS] = {10, 9, 8}; //piny wierszy
-byte colPins[COLS] = {13, 12, 11}; //piny kolum
- 
-char keys[ROWS][COLS] = { //mapowanie klawiatury
-  {'1','2','3'},
-  {'4','5','6'},
-  {'7','8','9'}
-};
- 
-Keypad klawiatura = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS ); //inicjalizacja klawiatury
-LiquidCrystal lcd(2, 3, 4, 5, 6, 7); //Informacja o podłączeniu nowego wyświetlacza
+byte rowPins[ROWS] = {10, 9, 8};   //rows pins
+byte colPins[COLS] = {13, 12, 11}; //columns pins
 
-void setup() {
+char keys[ROWS][COLS] = { //map keypad
+    {'1', '2', '3'},
+    {'4', '5', '6'},
+    {'7', '8', '9'}};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS); //initialize keypad
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);                                    //Create LCD screen object
+
+char capturedLetter = '0';
+
+int captureLetter();
+void updateScreen();
+
+void setup()
+{
   Serial.begin(9600);
-  lcd.begin(16, 2); //Deklaracja typu
-  lcd.setCursor(0, 0); //Ustawienie kursora
-  lcd.print("Hello"); //Wyświetlenie tekstu
-  lcd.setCursor(0, 1); //Ustawienie kursora
-  lcd.print("World!"); //Wyświetlenie tekstu
+  lcd.begin(16, 2);    //Declare type of LCD
+  lcd.setCursor(0, 0); //Set cursor
+  lcd.print("Hello");  //Show text
+  lcd.setCursor(0, 1); //Set cursor
+  lcd.print("World!"); //Show text
 }
 
-void loop() {
-  char klawisz = klawiatura.getKey();
-  
-  if (klawisz){
-    Serial.println(klawisz);
+void loop()
+{
+  if (captureLetter() == 1)
+  {
+    updateScreen();
   }
+}
+
+int captureLetter()
+{
+  char key = keypad.getKey();
+
+  if (key)
+  {
+    switch (key)
+    {
+    case '1':
+      capturedLetter = 'A';
+      break;
+    case '2':
+      capturedLetter = 'B';
+      break;
+    case '3':
+      capturedLetter = 'C';
+      break;
+    case '4':
+      capturedLetter = 'D';
+      break;
+    case '5':
+      capturedLetter = 'E';
+      break;
+    case '6':
+      capturedLetter = 'F';
+      break;
+    case '7':
+      capturedLetter = 'G';
+      break;
+    case '8':
+      capturedLetter = 'H';
+      break;
+    case '9':
+      capturedLetter = 'I';
+      break;
+    default:
+      break;
+    }
+    return 1;
+  }
+  else
+    return 0;
+}
+
+void updateScreen()
+{
+  lcd.clear();                  //Clear screen
+  lcd.setCursor(0, 0);          //Set cursor
+  lcd.print("Wybrana litera:"); //Show text
+  lcd.setCursor(0, 1);          //Set cursor
+  lcd.print(capturedLetter);    //Show text
 }
