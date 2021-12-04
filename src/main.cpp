19270d275495d14e3429898691fe55876afb720f
Szymon Hudziak <szymonh.dg@gmail.com>
2021-12-04 16:37:37 +0100

    Connect LCD

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+
+
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7); //Informacja o podłączeniu nowego wyświetlacza
 
 void setup() {
-  // put your setup code here, to run once:
+  lcd.begin(16, 2); //Deklaracja typu
+  lcd.setCursor(0, 0); //Ustawienie kursora
+  lcd.print("Hello"); //Wyświetlenie tekstu
+  lcd.setCursor(0, 1); //Ustawienie kursora
+  lcd.print("World!"); //Wyświetlenie tekstu
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
