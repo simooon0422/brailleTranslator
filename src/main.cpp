608135a95407e3fa71ea049b4d4a288bec9800a8
Szymon Hudziak <szymonh.dg@gmail.com>
2021-12-04 18:24:46 +0100

    Add code for controlling servos through I2C

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 #include <Keypad.h>
+#include <Adafruit_PWMServoDriver.h>
+
+
+#define SERVOMIN 150                // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX 550                // This is the 'maximum' pulse length count (out of 4096)
+#define SERVO_FREQ 50               // Analog servos run at ~50 Hz updates
 
 const byte ROWS = 3; //number of rows
 const byte COLS = 3; //number of columns
@@ -15,15 +21,23 @@ char keys[ROWS][COLS] = { //map keypad
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS); //initialize keypad
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);                                    //Create LCD screen object
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 char capturedLetter = '0';
 
 int captureLetter();
 void updateScreen();
+int degreesToPulse(int degrees);               //convert degrees to pulse
+void writePosition(int channel, int position); //control servo position
 
 void setup()
 {
   Serial.begin(9600);
+
+  pwm.begin();
+  pwm.setPWMFreq(SERVO_FREQ); // Analog servos run at ~50 Hz updates
+  delay(10);
+
   lcd.begin(16, 2);    //Declare type of LCD
   lcd.setCursor(0, 0); //Set cursor
   lcd.print("Hello");  //Show text
@@ -33,6 +47,8 @@ void setup()
 
 void loop()
 {
+  writePosition(0, 30); //Simple test of the circuit
+
   if (captureLetter() == 1)
   {
     updateScreen();
@@ -90,4 +106,20 @@ void updateScreen()
   lcd.print("Wybrana litera:"); //Show text
   lcd.setCursor(0, 1);          //Set cursor
   lcd.print(capturedLetter);    //Show text
+}
+
+int degreesToPulse(int degrees)
+{
+  if (degrees >= 0 && degrees <= 180)
+  {
+    int pulseLenght = map(degrees, 0, 180, SERVOMIN, SERVOMAX);
+    return pulseLenght;
+  }
+  else
+    return 0;
+}
+
+void writePosition(int channel, int newPosition)
+{
+  pwm.setPWM(channel, 0, degreesToPulse(newPosition));
 }
