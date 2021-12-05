8a8bc4b82493801d88ef8c10d93cf710a03b2e54
Szymon Hudziak <szymonh.dg@gmail.com>
2021-12-05 14:47:42 +0100

    Add more letters and comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
-#include <LiquidCrystal.h>
-#include <Keypad.h>
-#include <Adafruit_PWMServoDriver.h>
+#include <LiquidCrystal.h>           //LCD library
+#include <Keypad.h>                  //Keypad library
+#include <Adafruit_PWMServoDriver.h> //Servo driver library
 
 #define SERVOMIN 150  // This is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX 550  // This is the 'maximum' pulse length count (out of 4096)
@@ -10,8 +10,8 @@
 const byte ROWS = 3; //number of rows
 const byte COLS = 3; //number of columns
 
-byte rowPins[ROWS] = {10, 9, 8};   //rows pins
-byte colPins[COLS] = {13, 12, 11}; //columns pins
+byte rowPins[ROWS] = {10, 9, 8};   //rows' pins
+byte colPins[COLS] = {13, 12, 11}; //columns' pins
 
 char keys[ROWS][COLS] = { //map keypad
     {'1', '2', '3'},
@@ -20,27 +20,27 @@ char keys[ROWS][COLS] = { //map keypad
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS); //initialize keypad
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);                                    //Create LCD screen object
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();                //Create pwm object
 
 char capturedLetter = '0'; //Variable for converting signal from keypad to letter
 
 int brailleLetter[] = {0, 0, 0, 0, 0, 0}; //Braille representation of captured letter
-int servPositions[] = {0, 0, 0, 0, 0, 0};
+int servPositions[] = {0, 0, 0, 0, 0, 0}; //Stores servos' desired positions
 
-int captureLetter();
-void updateScreen();
-int degreesToPulse(int degrees);               //convert degrees to pulse
-void writePosition(int channel, int position); //control servo position
-void letterToBraille(char letter);
-void physicalRepresentation(int letterData[]);
-void convert(int a, int b, int c, int d, int e, int f);
-void updateServos();
+int captureLetter();                                    //Stores letter for conversion to Braille
+void updateScreen();                                    //Updates LCD screen to show current letter
+int degreesToPulse(int degrees);                        //convert degrees to pulse
+void writePosition(int channel, int position);          //control servo position
+void letterToBraille(char letter);                      //Converting chosen letter to its Braille version
+void convert(int a, int b, int c, int d, int e, int f); //
+void physicalRepresentation(int letterData[]);          //Fills array with servos' desired positions
+void updateServos();                                    //Updates servos' positions
 
 void setup()
 {
   Serial.begin(9600);
 
-  pwm.begin();
+  pwm.begin();                //Initialize pwm
   pwm.setPWMFreq(SERVO_FREQ); // Analog servos run at ~50 Hz updates
   delay(10);
 
@@ -49,28 +49,15 @@ void setup()
   lcd.print("Hello");  //Show text
   lcd.setCursor(0, 1); //Set cursor
   lcd.print("World!"); //Show text
-
-  Serial.println("Ready");
 }
 
 void loop()
 {
-  // writePosition(0, 0); //Simple test of the circuit
-  // writePosition(1, 0);
-
   if (captureLetter() == 1)
   {
     updateScreen();
     letterToBraille(capturedLetter);
     physicalRepresentation(brailleLetter);
-
-    // Serial.print(brailleLetter[0]);
-    // Serial.print(brailleLetter[1]);
-    // Serial.print(brailleLetter[2]);
-    // Serial.print(brailleLetter[3]);
-    // Serial.print(brailleLetter[4]);
-    // Serial.println(brailleLetter[5]);
-    // delay(1000);
   }
   updateServos();
   delay(1);
@@ -152,6 +139,30 @@ void letterToBraille(char letter)
   case 'A':
     convert(1, 0, 0, 0, 0, 0);
     break;
+  case 'B':
+    convert(1, 1, 0, 0, 0, 0);
+    break;
+  case 'C':
+    convert(1, 0, 0, 1, 0, 0);
+    break;
+  case 'D':
+    convert(1, 0, 0, 1, 1, 0);
+    break;
+  case 'E':
+    convert(1, 0, 0, 0, 1, 0);
+    break;
+  case 'F':
+    convert(1, 1, 0, 1, 0, 0);
+    break;
+  case 'G':
+    convert(1, 1, 0, 1, 1, 0);
+    break;
+  case 'H':
+    convert(1, 1, 0, 0, 1, 0);
+    break;
+  case 'I':
+    convert(0, 1, 0, 1, 0, 0);
+    break;
   default:
     convert(0, 0, 0, 0, 0, 0);
     break;
@@ -164,19 +175,19 @@ void physicalRepresentation(int letterData[])
   {
     if (letterData[i] == 1)
     {
-      servPositions[i] = 30;
+      servPositions[i] = 90;
     }
     else
       servPositions[i] = 0;
   }
 }
 
-void updateServos(){
+void updateServos()
+{
   for (int i = 0; i < 6; i++)
   {
     writePosition(i, servPositions[i]);
   }
-  
 }
 
 void convert(int a, int b, int c, int d, int e, int f)
