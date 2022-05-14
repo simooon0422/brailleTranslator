369da7f5fb577399cda5cd7e738b091205b5e659
Szymon Hudziak <szymonh.dg@gmail.com>
2022-05-14 12:42:41 +0200

    Add Bluetooth communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,20 @@
 #include <Arduino.h>
-#include <LiquidCrystal.h>           //LCD library
-#include <Keypad.h>                  //Keypad library
 #include <Adafruit_PWMServoDriver.h> //Servo driver library
+#include <SoftwareSerial.h> //Serial communiction library
 
 #define SERVOMIN 150  // This is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX 550  // This is the 'maximum' pulse length count (out of 4096)
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 
-const byte ROWS = 3; //number of rows
-const byte COLS = 3; //number of columns
-
-byte rowPins[ROWS] = {10, 9, 8};   //rows' pins
-byte colPins[COLS] = {13, 12, 11}; //columns' pins
-
-char keys[ROWS][COLS] = { //map keypad
-    {'1', '2', '3'},
-    {'4', '5', '6'},
-    {'7', '8', '9'}};
-
-Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS); //initialize keypad
-LiquidCrystal lcd(2, 3, 4, 5, 6, 7);                                    //Create LCD screen object
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();                //Create pwm object
+SoftwareSerial hc06(2,3);
 
-char capturedLetter = '0'; //Variable for converting signal from keypad to letter
+String message = "";
 
-int brailleLetter[] = {0, 0, 0, 0, 0, 0}; //Braille representation of captured letter
+int brailleDots[] = {0, 0, 0, 0, 0, 0}; //Braille representation of captured letter
 int servPositions[] = {0, 0, 0, 0, 0, 0}; //Stores servos' desired positions
 
-int captureLetter();                                    //Stores letter for conversion to Braille
+// int captureLetter();                                    //Stores letter for conversion to Braille
 void updateScreen();                                    //Updates LCD screen to show current letter
 int degreesToPulse(int degrees);                        //convert degrees to pulse
 void writePosition(int channel, int position);          //control servo position
@@ -40,81 +27,93 @@ void setup()
 {
   Serial.begin(9600);
 
+  Serial.println("ENTER AT Commands:");
+  //Initialize Bluetooth Serial Port
+  hc06.begin(9600);
+
   pwm.begin();                //Initialize pwm
   pwm.setPWMFreq(SERVO_FREQ); // Analog servos run at ~50 Hz updates
   delay(10);
 
-  lcd.begin(16, 2);    //Declare type of LCD
-  lcd.setCursor(0, 0); //Set cursor
-  lcd.print("Hello");  //Show text
-  lcd.setCursor(0, 1); //Set cursor
-  lcd.print("World!"); //Show text
 }
 
 void loop()
 {
-  if (captureLetter() == 1)
-  {
-    updateScreen();
-    letterToBraille(capturedLetter);
-    physicalRepresentation(brailleLetter);
-  }
-  updateServos();
-  delay(1);
-}
-
-int captureLetter()
-{
-  char key = keypad.getKey();
-
-  if (key)
-  {
-    switch (key)
-    {
-    case '1':
-      capturedLetter = 'A';
-      break;
-    case '2':
-      capturedLetter = 'B';
-      break;
-    case '3':
-      capturedLetter = 'C';
-      break;
-    case '4':
-      capturedLetter = 'D';
-      break;
-    case '5':
-      capturedLetter = 'E';
-      break;
-    case '6':
-      capturedLetter = 'F';
-      break;
-    case '7':
-      capturedLetter = 'G';
-      break;
-    case '8':
-      capturedLetter = 'H';
-      break;
-    case '9':
-      capturedLetter = 'I';
-      break;
-    default:
-      break;
+  //Write data from HC06 to Serial Monitor
+  if (hc06.available()) {
+    //Serial.write(hc06.read());
+    message = hc06.readString();
+    Serial.println(message);
     }
-    return 1;
+
+  //Write from Serial Monitor to HC06
+  if (Serial.available()) {
+    hc06.write(Serial.read());
   }
-  else
-    return 0;
+  
+  if (message == "hello") {
+     Serial.println("It works!");
+     message = "";
+  }
+
+  //Write from Serial Monitor to HC06
+  // if (Serial.available()) {
+  //   hc06.write(Serial.read());
+  // }
+
+  // if (captureLetter() == 1)
+  // {
+  //   updateScreen();
+  //   letterToBraille(capturedLetter);
+  //   physicalRepresentation(brailleLetter);
+  // }
+  // updateServos();
+  // delay(1);
 }
 
-void updateScreen()
-{
-  lcd.clear();                  //Clear screen
-  lcd.setCursor(0, 0);          //Set cursor
-  lcd.print("Wybrana litera:"); //Show text
-  lcd.setCursor(0, 1);          //Set cursor
-  lcd.print(capturedLetter);    //Show text
-}
+// int captureLetter()
+// {
+//   char key = keypad.getKey();
+
+//   if (key)
+//   {
+//     switch (key)
+//     {
+//     case '1':
+//       capturedLetter = 'A';
+//       break;
+//     case '2':
+//       capturedLetter = 'B';
+//       break;
+//     case '3':
+//       capturedLetter = 'C';
+//       break;
+//     case '4':
+//       capturedLetter = 'D';
+//       break;
+//     case '5':
+//       capturedLetter = 'E';
+//       break;
+//     case '6':
+//       capturedLetter = 'F';
+//       break;
+//     case '7':
+//       capturedLetter = 'G';
+//       break;
+//     case '8':
+//       capturedLetter = 'H';
+//       break;
+//     case '9':
+//       capturedLetter = 'I';
+//       break;
+//     default:
+//       break;
+//     }
+//     return 1;
+//   }
+//   else
+//     return 0;
+// }
 
 int degreesToPulse(int degrees)
 {
@@ -192,10 +191,10 @@ void updateServos()
 
 void convert(int a, int b, int c, int d, int e, int f)
 {
-  brailleLetter[0] = a;
-  brailleLetter[1] = b;
-  brailleLetter[2] = c;
-  brailleLetter[3] = d;
-  brailleLetter[4] = e;
-  brailleLetter[5] = f;
+  brailleDots[0] = a;
+  brailleDots[1] = b;
+  brailleDots[2] = c;
+  brailleDots[3] = d;
+  brailleDots[4] = e;
+  brailleDots[5] = f;
 }
